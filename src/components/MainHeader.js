c04e3b6b3412756580e9b08f68ddf522cf9faec4
Phillip Lorenzo <phillip.lorenzo@xy.company>
2019-07-29 11:20:58 -0700

    style: update UI style for redesign (#53)

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -9,7 +9,7 @@ import {
   Collapse
 } from 'reactstrap'
 
-import logo from '../XYODevelopers_XYO_Logo.png'
+import logo from '../XYO_full_colored.png'
 
 export default class MainHeader extends Component {
   constructor(props) {
@@ -29,23 +29,23 @@ export default class MainHeader extends Component {
 
   render() {
     return (
-      <div className="bg-navy">
+      <div className="bg-light">
         <Navbar expand="md" className="container navbar-dark">
           <NavbarBrand href="http://developers.xyo.network/" className="text-white"><img src={logo} alt="XYO Dev Portal" /></NavbarBrand>
             <NavbarToggler onClick={this.toggle} />
               <Collapse isOpen={this.state.isOpen} navbar>  
                 <Nav navbar className="ml-auto navbar-inverse bg-inverse">
                   <NavItem>
-                  <NavLink href="https://developers.xyo.network/docs/en/getting-started/" className="text-white" >Getting Started</NavLink>
+                  <NavLink href="https://developers.xyo.network/docs/en/getting-started/" className="text-dark" >GETTING STARTED</NavLink>
                   </NavItem>
                   <NavItem>
-                  <NavLink href="https://developers.xyo.network/docs/en/Introduction/" className="text-white">Guides</NavLink>
+                  <NavLink href="https://developers.xyo.network/docs/en/Introduction/" className="text-dark">GUIDES</NavLink>
                   </NavItem>
                   <NavItem>
-                  <NavLink href="http://reference.xyo.network/" className="text-white">Reference</NavLink>
+                  <NavLink href="http://reference.xyo.network/" className="text-dark">REFERENCE</NavLink>
                   </NavItem>
                   <NavItem>
-                    <NavLink href="https://developers.xyo.network/help/" className="text-white" target="_blank">Help</NavLink>
+                    <NavLink href="https://developers.xyo.network/help/" className="text-dark" target="_blank">HELP</NavLink>
                   </NavItem>
                 </Nav>
             </Collapse>
